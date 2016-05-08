4c887e2de012d4a0bbf06af6a6836f8a8605f0c4
nkhumphreys <nkhumphreys@gmail.com>
2016-05-08 14:48:40 +0000

    Fix old intranet name in file list

--- setup_project.sh
+++ setup_project.sh
@@ -13,7 +13,7 @@ if [ -z $PROJECT_NAME ]; then usage; exit 1; fi
 echo "Setting up project: $PROJECT_NAME"
 
 # list of all files that contain template code for project setup
-files=( conf/entrypoint.sh conf/supervisor.intranet.conf conf/uwsgi.ini docker-compose.yml Dockerfile project_dir/urls.py project_dir/settings.py project_dir/wsgi.py project_dir/templates/base.html project_dir/templates/index.html )
+files=( conf/entrypoint.sh conf/supervisor.project_name.conf conf/uwsgi.ini docker-compose.yml Dockerfile project_dir/urls.py project_dir/settings.py project_dir/wsgi.py project_dir/templates/base.html project_dir/templates/index.html )
 
 # Iterate files and replace template code with the project name
 for f in "${files[@]}"
