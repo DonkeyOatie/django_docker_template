43e00341368705acb5a678123af50f1bc24ff4cf
nkhumphreys <nkhumphreys@gmail.com>
2016-05-08 14:18:24 +0000

    Fix setup_project bash script
    The script was too relaxed about what it changed and broke the git
    index, we know which files contain template code so just run the sed
    operation on the known template code files

--- setup_project.sh
+++ setup_project.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export LC_ALL=C
 
 usage() {
     echo """
@@ -7,12 +8,20 @@ usage() {
 }
 
 PROJECT_NAME=$1
-
 if [ -z $PROJECT_NAME ]; then usage; exit 1; fi
 
 echo "Setting up project: $PROJECT_NAME"
 
-LC_ALL=C find ./ -type f -exec sed -i '' -e "s/<project_name>/$PROJECT_NAME/" {} \;
+# list of all files that contain template code for project setup
+files=(conf/entrypoint.sh conf/supervisor.intranet.conf conf/uwsgi.ini docker-compose.yml Dockerfile project_dir/urls.py project_dir/settings.py project_dir/wsgi.py)
+
+# Iterate files and replace template code with the project name
+for f in "${files[@]}"
+do
+    sed -i '' -e "s/<project_name>/$PROJECT_NAME/" ${f}
+done
+
+# Finally move project dir to new name
 mv project_dir $PROJECT_NAME
 
 echo "Finished setting up Project, get to work!"
