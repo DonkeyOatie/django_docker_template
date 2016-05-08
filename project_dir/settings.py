2fd4f4fbef579704f0953d41a915f3929089de82
Didi Hoffmann <didi@ribalba.de>
2016-05-08 15:05:51 +0000

    Added the secret key placeholder to the settings file

--- project_dir/settings.py
+++ project_dir/settings.py
@@ -23,7 +23,7 @@ def here(x):
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fc1f8c78-152d-11e6-ad26-acbc328c4933'
+SECRET_KEY = 'SECRET_KEY_PLACEHOLDER'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
